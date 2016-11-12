e072816f01bcdf848ba311cf8aa469373fdf0389
VincenzoGambino <gambinovin@gmail.com>
2016-11-12 14:37:18 +0000

    added LaraTokServiceProvider

---
+++ src/LaraTokServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace VincenzoGambino\LaraTok;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraTokServiceProvider extends ServiceProvider
+{
+  /**
+   * Bootstrap the application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    // Route file
+    include __DIR__.'/Routes/web.php';
+
+    // Migrations
+    $this->loadMigrationsFrom(__DIR__.'/Migrations');
+
+    // Publish Configuration
+    $this->publishes([
+      __DIR__.'/Config/laratok.php' => config_path('laratok.php'),
+    ], 'config');
+  }
+
+  /**
+   * Register the application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+
+    // Config
+    $this->mergeConfigFrom( __DIR__ . '/Config/laratok.php', 'laratok');
+
+    $this->loadViewsFrom(__DIR__ . '/Views', 'laratok');
+
+    $this->app->bind('vincenzogambino-laratok', function() {
+      return new LaraTok;
+    });
+  }
+}
